09ad4b17a35ee015b6e77d536e8efa69e9e398f4
Eicker, Alan <alan.eicker@allstate.com>
2019-02-25 13:26:31 -0600

    updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/', routes);
 app.use(bodyParser.json());
+app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Node server listening on port ${port}`)
