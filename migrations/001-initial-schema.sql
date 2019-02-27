39c7393b26007ea148704b5ae7fb4116f518a1e0
Eicker, Alan <alan.eicker@allstate.com>
2019-02-27 13:43:57 -0600

    Removes postman config. Updates 001-initial-schema.sql

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -4,14 +4,20 @@
 
 CREATE TABLE Users (
   id INTEGER PRIMARY KEY, 
+  name VARCHAR,
+  email VARCHAR,
   username VARCHAR, 
   password VARCHAR
 );
 
 INSERT INTO Users (
+  name,
+  email,
   username, 
   PASSWORD
 ) VALUES (
+  'Alan Eicker',
+  'alaneicker@gmail.com',
   'alaneicker',
   'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
 );
