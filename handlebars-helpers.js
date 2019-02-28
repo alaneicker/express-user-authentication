6aa2508e8eb21040347dd6e29416611ee6f4a895
Eicker, Alan <alan.eicker@allstate.com>
2019-02-28 11:13:25 -0600

    Update handlebars-helpers.js

--- handlebars-helpers.js
+++ handlebars-helpers.js
@@ -7,13 +7,13 @@ module.exports = {
   },
   block(name) {
     const blocks  = this._blocks,
-        content = blocks && blocks[name];
+          content = blocks && blocks[name];
 
     return content ? content.join("\n") : null;
   },
   contentFor(name, options) {
     const blocks = this._blocks || (this._blocks = {}),
-        block  = blocks[name] || (blocks[name] = []);
+          block  = blocks[name] || (blocks[name] = []);
 
     block.push(options.fn(this));
   }
