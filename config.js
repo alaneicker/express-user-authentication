8f361cbb28c490ec13c19989733a62e3ead8b9c1
Alan Eicker <alaneicker@hotmail.com>
2019-02-28 14:45:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const generateTokenSignature = () => {
-  var signature = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var signature = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
   for (var i = 0; i < 20; i++) {
     signature += possible.charAt(Math.floor(Math.random() * possible.length));
