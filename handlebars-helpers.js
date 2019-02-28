ebfecdb624bf5294e20317afc1cb0c5ed4e7d51d
Eicker, Alan <alan.eicker@allstate.com>
2019-02-28 11:12:20 -0600

    Adds content and block helpers

--- handlebars-helpers.js
+++ handlebars-helpers.js
@@ -1,8 +1,20 @@
 module.exports = {
-  ifeq: function(a, b, options){
+  ifeq(a, b, options) {
     if (a === b) {
       return options.fn(this);
     }
     return options.inverse(this);
   },
+  block(name) {
+    const blocks  = this._blocks,
+        content = blocks && blocks[name];
+
+    return content ? content.join("\n") : null;
+  },
+  contentFor(name, options) {
+    const blocks = this._blocks || (this._blocks = {}),
+        block  = blocks[name] || (blocks[name] = []);
+
+    block.push(options.fn(this));
+  }
 }
